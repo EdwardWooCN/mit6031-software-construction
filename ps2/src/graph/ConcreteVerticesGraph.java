5456f56abd884ce7f7b1ef2eeee3166551593b17
edward <1248999697@qq.com>
2022-10-14 02:07:15 +0800

    finish ps2 problem2.2

--- ps2/src/graph/ConcreteVerticesGraph.java
+++ ps2/src/graph/ConcreteVerticesGraph.java
@@ -3,10 +3,8 @@
  */
 package graph;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * An implementation of Graph.
@@ -18,42 +16,109 @@ public class ConcreteVerticesGraph implements Graph<String> {
     private final List<Vertex> vertices = new ArrayList<>();
     
     // Abstraction function:
-    //   TODO
+    //   Vertex represents vertices of graph and (Vertex.source -> Vertex.this) && (Vertex.this -> Vertex.target) represents edge
     // Representation invariant:
-    //   TODO
+    //   unique Vertex.id in vertices
     // Safety from rep exposure:
-    //   TODO
+    //   vertices use unmodifiable view
     
-    // TODO constructor
+    ConcreteVerticesGraph() {}
     
-    // TODO checkRep
+    private void checkRep() {
+        assert new HashSet<Vertex>(vertices).size() == vertices().size();
+    }
     
     @Override public boolean add(String vertex) {
-        throw new RuntimeException("not implemented");
+        if (Objects.nonNull(findVertexInList(vertex))) {
+            checkRep();
+            return false;
+        }
+
+        vertices.add(Vertex.getInstance(vertex));
+        checkRep();
+        return true;
     }
     
     @Override public int set(String source, String target, int weight) {
-        throw new RuntimeException("not implemented");
+        //remove edge
+        if (weight == 0) {
+            Vertex srcVertex = findVertexInList(source);
+            int res1 = 0;
+            if (Objects.nonNull(srcVertex)) {
+                res1 = srcVertex.removeEdgeFromTarget(target);
+            }
+            Vertex trgVertex = findVertexInList(target);
+            if (Objects.nonNull(trgVertex) && !source.equals(target)) {
+                int res2 = trgVertex.removeEdgeFromSource(source);
+                assert res1 == res2;
+            }
+            checkRep();
+            return res1;
+        }
+
+        //add or change edge
+        add(source);
+        add(target);
+        int res1 = findVertexInList(target).updateEdgeFromSource(source, weight);
+        if (!source.equals(target)) {
+            int res2 = findVertexInList(source).updateEdgeFromTarget(target, weight);
+            assert res2 == res1;
+        }
+        checkRep();
+        return res1;
     }
     
     @Override public boolean remove(String vertex) {
-        throw new RuntimeException("not implemented");
+        //remove vertex
+        Vertex removedVertex = findVertexInList(vertex);
+        if (Objects.isNull(removedVertex)) {
+            checkRep();
+            return false;
+        }
+
+        //delete edge(other -> vertex)
+        removedVertex.getSourceEdges().keySet().forEach(other -> findVertexInList(other).removeEdgeFromTarget(vertex));
+        //delete edge(vertex -> other)
+        removedVertex.getTargetEdges().keySet().forEach(other -> findVertexInList(other).removeEdgeFromSource(vertex));
+        assert deleteVertexInList(vertex);
+        checkRep();
+        return true;
     }
     
     @Override public Set<String> vertices() {
-        throw new RuntimeException("not implemented");
+        return vertices.stream().map(Vertex::getId).collect(Collectors.toSet());
     }
     
     @Override public Map<String, Integer> sources(String target) {
-        throw new RuntimeException("not implemented");
+        return Optional.ofNullable(findVertexInList(target)).map(Vertex::getSourceEdges).orElse(new HashMap<>());
     }
     
     @Override public Map<String, Integer> targets(String source) {
-        throw new RuntimeException("not implemented");
+        return Optional.ofNullable(findVertexInList(source)).map(Vertex::getTargetEdges).orElse(new HashMap<>());
     }
     
-    // TODO toString()
-    
+    private Vertex findVertexInList(String id) {
+        return vertices.stream().filter(item -> item.getId().equals(id)).findAny().orElse(null);
+    }
+
+    private boolean deleteVertexInList(String id) {
+        Iterator<Vertex> vertexIterator = vertices.listIterator();
+        while (vertexIterator.hasNext()) {
+            Vertex vertex = vertexIterator.next();
+            if (vertex.getId().equals(id)) {
+                vertexIterator.remove();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "ConcreteVerticesGraph{" +
+                "vertices=" + vertices +
+                '}';
+    }
 }
 
 /**
@@ -65,22 +130,114 @@ public class ConcreteVerticesGraph implements Graph<String> {
  * up to you.
  */
 class Vertex {
-    
-    // TODO fields
-    
+    private final String id;
+
+    //key is the source of edge, target is this vertex, i.e. (source -> this, weight)
+    private final Map<String, Integer> sourceEdges = new HashMap<>();
+
+    //key is the target of edge, source is this vertex, i.e. (this -> target, weight)
+    private final Map<String, Integer> targetEdges = new HashMap<>();
+
     // Abstraction function:
-    //   TODO
+    //   id is the node, (source -> this, weight) && (this -> target, weight) are edge related to this node
     // Representation invariant:
-    //   TODO
+    //   this node cannot be contained in sourcesEdge
     // Safety from rep exposure:
-    //   TODO
+    //   sourcesEdge, targetsEdge use unmodifiable view
     
-    // TODO constructor
-    
-    // TODO checkRep
-    
-    // TODO methods
-    
-    // TODO toString()
-    
+    private Vertex(String id) {
+        assert Objects.nonNull(id);
+        this.id = id;
+    }
+
+    public static Vertex getInstance(String id) {
+        return new Vertex(id);
+    }
+
+    private void checkRep() {
+        //this node cannot be contained in sourcesEdge
+        assert !sourceEdges.containsKey(id);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public Map<String, Integer> getSourceEdges() {
+        return Collections.unmodifiableMap(sourceEdges);
+    }
+
+    public Map<String, Integer> getTargetEdges() {
+        return Collections.unmodifiableMap(targetEdges);
+    }
+
+    /**
+     * update edge (source -> this, weight)
+     * @param source: id of source vertex
+     * @param weight: edge weight, has positive value
+     * @return: previous weight of this edge, zero if non-existed before
+     */
+    public int updateEdgeFromSource(String source, int weight) {
+        assert weight > 0 : "invalid weight";
+        assert !source.equals(id) : "this node cannot be contained in sourceEdges, should be in targetEdges";
+
+        Integer oldWeight = sourceEdges.get(source);
+        sourceEdges.put(source, weight);
+        return Optional.ofNullable(oldWeight).orElse(0);
+    }
+
+    /**
+     * update edge (this -> target, weight)
+     * @param target: id of target vertex
+     * @param weight: edge weight, has positive value
+     * @return: previous weight of this edge, zero if non-existed before
+     */
+    public int updateEdgeFromTarget(String target, int weight) {
+        assert weight > 0 : "invalid weight";
+
+        Integer oldWeight = targetEdges.get(target);
+        targetEdges.put(target, weight);
+        return Optional.ofNullable(oldWeight).orElse(0);
+    }
+
+    /**
+     * remove edge (source -> this, weight)
+     * @param source: id of source vertex
+     * @return: previous weight of this edge, zero if non-existed before
+     */
+    public int removeEdgeFromSource(String source) {
+        return Optional.ofNullable(sourceEdges.remove(source)).orElse(0);
+    }
+
+    /**
+     * remove edge (this -> target, weight)
+     * @param target: id of target vertex
+     * @return: previous weight of this edge, zero if non-existed before
+     */
+    public int removeEdgeFromTarget(String target) {
+        return Optional.ofNullable(targetEdges.remove(target)).orElse(0);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Vertex:\nsourcesEdge:%s\ntargetsEdge:%s", edgesExpressionOf(sourceEdges, true), edgesExpressionOf(targetEdges, false));
+    }
+
+    private String edgesExpressionOf(Map<String, Integer> edges, boolean isSourceEdge) {
+        List<String> result = new LinkedList<>();
+        for (Map.Entry<String, Integer> entry : edges.entrySet()) {
+            String edgeExpr;
+            if (isSourceEdge) {
+                edgeExpr = edgeExpressionOf(entry.getKey(), this.id, entry.getValue());
+            } else {
+                edgeExpr = edgeExpressionOf(this.id, entry.getKey(), entry.getValue());
+            }
+            result.add(edgeExpr);
+        }
+        return result.toString();
+    }
+
+    private String edgeExpressionOf(String source, String target, int weight) {
+        return String.format("Edge=(%s -> %s, weight=%d)", source, target, weight);
+    }
 }
--- ps2/test/graph/ConcreteVerticesGraphTest.java
+++ ps2/test/graph/ConcreteVerticesGraphTest.java
@@ -5,7 +5,10 @@ package graph;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Arrays;
 
 /**
  * Tests for ConcreteVerticesGraph.
@@ -29,17 +32,82 @@ public class ConcreteVerticesGraphTest extends GraphInstanceTest {
      */
     
     // Testing strategy for ConcreteVerticesGraph.toString()
-    //   TODO
-    
-    // TODO tests for ConcreteVerticesGraph.toString()
+    public void testToString() {
+        Graph<String> graph = emptyInstance();
+
+        assertTrue(graph.toString().contains("ConcreteVerticesGraph{vertices="));
+    }
     
     /*
      * Testing Vertex...
      */
     
     // Testing strategy for Vertex
-    //   TODO
-    
+    //   updateEdgeFromSource:
+    //     op: add, change
+    //   updateEdgeFromTarget:
+    //     op: add, change
+    //   removeEdgeFromSource:
+    //     result: 0(not existed), >0(exist)
+    //   removeEdgeFromTarget:
+    //     result: 0(not existed), >0(exist)
+
     // TODO tests for operations of Vertex
-    
+    @Test
+    public void testUpdateEdgeFromSource() {
+        Vertex v1 = Vertex.getInstance(vertex1);
+
+        int firstEdgeWeight = 2;
+        int oldWeight = v1.updateEdgeFromSource(vertex2, firstEdgeWeight);
+        assertSame("expect add edge", 0, oldWeight);
+        assertSame("expect in data", firstEdgeWeight, v1.getSourceEdges().getOrDefault(vertex2, -1));
+
+        int secondEdgeWeight = 5;
+        oldWeight = v1.updateEdgeFromSource(vertex2, secondEdgeWeight);
+        assertSame("expect change edge", firstEdgeWeight, oldWeight);
+        assertSame("expect in data", secondEdgeWeight, v1.getSourceEdges().getOrDefault(vertex2, -1));
+    }
+
+    @Test
+    public void testUpdateEdgeFromTarget() {
+        Vertex v1 = Vertex.getInstance(vertex1);
+
+        int firstEdgeWeight = 2;
+        int oldWeight = v1.updateEdgeFromTarget(vertex2, firstEdgeWeight);
+        assertSame("expect add edge", 0, oldWeight);
+        assertSame("expect in data", firstEdgeWeight, v1.getTargetEdges().getOrDefault(vertex2, -1));
+
+        int secondEdgeWeight = 5;
+        oldWeight = v1.updateEdgeFromTarget(vertex2, secondEdgeWeight);
+        assertSame("expect change edge", firstEdgeWeight, oldWeight);
+        assertSame("expect in data", secondEdgeWeight, v1.getTargetEdges().getOrDefault(vertex2, -1));
+    }
+
+    @Test
+    public void testRemoveEdgeFromSource() {
+        Vertex v1 = Vertex.getInstance(vertex1);
+
+        int oldWeight = v1.removeEdgeFromSource(vertex2);
+        assertSame("expect remove non-existed edge", 0, oldWeight);
+
+        int firstEdgeWeight = 2;
+        v1.updateEdgeFromSource(vertex2, firstEdgeWeight);
+        oldWeight = v1.removeEdgeFromSource(vertex2);
+        assertSame("expect remove existed edge", firstEdgeWeight, oldWeight);
+        assertTrue("expect in data", v1.getSourceEdges().isEmpty());
+    }
+
+    @Test
+    public void testRemoveEdgeFromTarget() {
+        Vertex v1 = Vertex.getInstance(vertex1);
+
+        int oldWeight = v1.removeEdgeFromTarget(vertex2);
+        assertSame("expect remove non-existed edge", 0, oldWeight);
+
+        int firstEdgeWeight = 2;
+        v1.updateEdgeFromTarget(vertex2, firstEdgeWeight);
+        oldWeight = v1.removeEdgeFromTarget(vertex2);
+        assertSame("expect remove existed edge", firstEdgeWeight, oldWeight);
+        assertTrue("expect in data", v1.getTargetEdges().isEmpty());
+    }
 }
